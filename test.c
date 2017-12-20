4cf2d66bac48b5b4ec02bac4b73325999e6ed58f
YeonJung Seo <s980107@naver.com>
2017-12-21 04:04:43 +0900

    first test.c

--- test.c
+++ test.c
@@ -1,18 +1,6 @@
 #include <stdio.h>
 
-static void  
-print_line_number(const char *input_path)  
-{  
-	for (int i = 0; i < 110; ++i) {
-        printf("%3d --> \033[%dm%s\033[0m\n", i, i, "Text");
-	printf("Hello Systemsoftware");
-	printf("Plsease~");
-	printf("=========\nHello World! I'm DongHee\n==========");
-	printf("Nice to meet you, I'm Hyemin");
-	printf("How are you?");    
-}
-}  
-  
+
 int  
 main(int argc, char *argv[])  
 {  
@@ -20,7 +8,7 @@ main(int argc, char *argv[])
 		printf("Usage: lineno <input file>\n");  
 		return 1;  
 	}  
-	print_line_number(argv[1]);
+	printf(" I am yeon jung!");
 	printf("Hello ");  
 	return 0;  
 }  
