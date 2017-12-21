8145b5a6ec7e77a5dd9c4469f39f2c11de1c42c2
SooBin <sbpark422@naver.com>
2017-12-21 13:00:29 +0900

    soobin test.c

--- test.c
+++ test.c
@@ -1,24 +1,16 @@
- -main()
- -{
- -}
- +#include <stdio.h>  
- +
- +static void  
- +print_line_number(const char *input_path)  
- +{  
- +	for (int i = 0; i < 110; ++i) {
- +        printf("%3d --> \033[%dm%s\033[0m\n", i, i, "Text");
- +    }
- +}  
- +  
- +int  
- +main(int argc, char *argv[])  
- +{  
- +	if (argc < 1) {  
- +		printf("Usage: lineno <input file>\n");  
- +		return 1;  
- +	}  
- +	print_line_number(argv[1]);  
- +	return 0;  
- +}  
- +
+#include <stdio.h>
+
+
+int  
+main(int argc, char *argv[])  
+{  
+	if (argc < 1) {  
+		printf("Usage: lineno <input file>\n");  
+		return 1;  
+	}  
+	printf(" I am soobin");
+	printf("Hello ");  
+	return 0;  
+}  
+
+
