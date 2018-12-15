ffe5a966112da66a96af086543ceb50bb071d878
KyungWoon Cho <cezanne@codemayo.com>
2018-12-15 20:03:08 +0900

    소스 트리 다시 정리
    - 불필요한 파일 삭제
    - lineno.c 정리

--- lineno.c
+++ lineno.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-static void // 1773139 장다솜
+static void
 print_line_number(const char *input_path)
 {
 	FILE *fp; 
 	
-	fp = fopen(input_path, "r"); //파일 입출력
-	if (fp != NULL){ //파일의 끝까지 읽어서 
+	fp = fopen(input_path, "r");
+	if (fp != NULL){
 		char	buf[1024];
 		int	lineno = 1;
 		while(fgets(buf, 1024, fp)) {
 			printf("%u: %s", lineno, buf);
-			lineno++; //줄 번호 출력
+			lineno++;
 		}
 		fclose(fp);
 	}
-       else{ //파일을 열 수 없음
+       else{
                 printf("Cannt open file!\n");
                 exit(1);
        }
@@ -29,8 +29,6 @@ main(int argc, char *argv[])
 		printf("Usag: lineno <input file>\n");
 		return 1;
 	}
-        printf("This is a program printing line of the read file\n");
 	print_line_number(argv[1]);
-        
 	return 0;
 }
