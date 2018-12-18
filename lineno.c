6ff5f30e4bd3872d89e3b26384d055f746427f08
YUJE0NG <uj2741@gmail.com>
2018-12-18 11:48:15 +0900

    Add comment

--- lineno.c
+++ lineno.c
@@ -30,9 +30,14 @@ main(int argc, char *argv[])
 		printf("Usage: lineno <input file>\n");
                 return 1;
 	}
+<<<<<<< 172ceb236afc2498d44152bb8f5c1da03afe0a13
 	printf("I am Jeihee Cho!\n");
 	printf("HyeWon Sung\n");
 	printf("Eunjung Lim\n");
+=======
+	printf("YUJEONG LEE\n");
+>>>>>>> Add comment
 	print_line_number(argv[1]);
+	printf("\nThanks\n");
 	return 0;
 }
